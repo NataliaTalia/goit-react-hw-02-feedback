8b4a60c7766cf05152c13f32274b016714227766
Natalia <nataliamaliovana.nm@gmail.com>
2023-05-07 17:49:30 +0700

    first commit

---
+++ src/components/Feedback/Statistics.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Statistics = ({
+  title,
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total feedback: {total}</p>
+      <p>Positive Feedback: {positivePercentage}</p>
+    </div>
+  );
+};
+
+export default Statistics;
+
+Statistics.propTypes = {
+  title: PropTypes.string.isRequired,
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+};
